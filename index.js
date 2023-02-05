cffc0dfad22b4a1df0a4847dc1a25108f454a8c6
JosuaHall <halljosua@web.de>
2023-02-04 20:29:57 -0600

    new client on netlify

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 
 const corsOptions = {
-  origin: "https://athletia.onrender.com",
+  origin: "https://athletia.netlify.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
